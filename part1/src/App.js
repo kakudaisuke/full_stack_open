9c502944cc373e46463037eba5e7964be4ce616d
kakudaisuke <dk.15step@gmail.com>
2021-09-28 21:15:44 +0900

    1.7: unicafe step2まで

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,79 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Header = (prop) => {
-  const course = prop.course
-
+const Title = ({ text }) => {
   return (
-    <h1>{course}</h1>
+    <h1>{text}</h1>
   )
 }
 
-const Part = (props) => {
-  const part = props.part
-  const exercises = props.exercises
-
+const Button = (props) => {
   return (
-    <>
-      <p>
-        {part} {exercises}
-      </p>
-    </>
-  )
-  
-}
-
-const Content = (props) => {
-  const name1 = props.parts[0].name
-  const name2 = props.parts[1].name
-  const name3 = props.parts[2].name
-  const exercises1 = props.parts[0].exercises
-  const exercises2 = props.parts[1].exercises
-  const exercises3 = props.parts[2].exercises
-
-
-  return (
-    <>
-      <Part part={name1} exercises={exercises1} />
-      <Part part={name2} exercises={exercises2} />
-      <Part part={name3} exercises={exercises3} />
-    </>
+    <button onClick={props.handleClick}>{props.text}</button>
   )
 }
 
-const Total = (props) => {
-  const exercises1 = props.parts[0].exercises
-  const exercises2 = props.parts[1].exercises
-  const exercises3 = props.parts[2].exercises
-
+const Statistic = (props) => {
   return (
-    <>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </>
+    <p>{props.text} {props.score}{props.unit}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const sum = good + neutral + bad
+  const goodP = good * 1
+  const neutralP = neutral * 0
+  const badP = bad * -1
 
   return (
     <>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Title text="give feedback" />
+      <Button text="good" handleClick={()=> setGood(good + 1)} />
+      <Button text="neutral" handleClick={()=> setNeutral(neutral + 1)} />
+      <Button text="good" handleClick={()=> setBad(bad + 1)} />
+      <Title text="statistics" />
+      <Statistic text="good" score={good} />
+      <Statistic text="neutral" score={neutral} />
+      <Statistic text="bad" score={bad} />
+      <Statistic text="all" score={sum} />
+      <Statistic text="average" score={(goodP + neutralP + badP) / sum} />
+      <Statistic text="positive" score={good / sum} unit=" %" />
     </>
   )
 }
