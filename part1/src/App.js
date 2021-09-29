634ef27428d7fa8b67a527943e032ebef04ca8b2
kakudaisuke <dk.15step@gmail.com>
2021-09-29 21:33:05 +0900

    1.9まで

--- part1/src/App.js
+++ part1/src/App.js
@@ -18,16 +18,38 @@ const Statistic = (props) => {
   )
 }
 
+const Statistics = (props) => {
+  const good = props.good
+  const neutral = props.neutral
+  const bad = props.bad
+  const all = props.all
+  const av = (good.score + neutral.score + bad.score) / all.count || 0
+  const positive = good / all.count
+
+  if (all.count === 0) return "No feedback given";
+
+  return (
+    <>
+      <p>{good.text} {good.score}</p>
+      <p>{neutral.text} {neutral.score}</p>
+      <p>{bad.text} {bad.score}</p>
+      <p>{all.text} {all.count}</p>
+      <p>average {av} %</p>
+      <p>positive {bad.score} %</p>
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const sum = good + neutral + bad
-  const goodP = good * 1
-  const neutralP = neutral * 0
-  const badP = bad * -1
+  const all_obj = {text: "all", count: good + neutral + bad}
+  const good_obj = {text: "good", score: good * 1}
+  const neutral_obj = {text: "neutral", score: neutral * 0}
+  const bad_obj = {text: "bad", score: bad * -1}
 
   return (
     <>
@@ -36,12 +58,7 @@ const App = () => {
       <Button text="neutral" handleClick={()=> setNeutral(neutral + 1)} />
       <Button text="good" handleClick={()=> setBad(bad + 1)} />
       <Title text="statistics" />
-      <Statistic text="good" score={good} />
-      <Statistic text="neutral" score={neutral} />
-      <Statistic text="bad" score={bad} />
-      <Statistic text="all" score={sum} />
-      <Statistic text="average" score={(goodP + neutralP + badP) / sum} />
-      <Statistic text="positive" score={good / sum} unit=" %" />
+      <Statistics good={good_obj} neutral={neutral_obj} bad={bad_obj} all={all_obj} />
     </>
   )
 }
