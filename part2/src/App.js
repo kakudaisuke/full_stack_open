cd273977967173006240644e2b40b33fc82b6957
kakudaisuke <dk.15step@gmail.com>
2021-10-10 15:39:29 +0900

    start part2

---
+++ part2/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const Header = ({ name }) => {
+  return <h1>{name}</h1>
+}
+
+
+const Course = ({ parts }) => {
+  return (
+    <>
+      <p>{parts[0].name} {parts[0].exercises}</p>
+      <p>{parts[1].name} {parts[1].exercises}</p>
+      <p>{parts[2].name} {parts[2].exercises}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <>
+      <Header name={course.name} />
+      <Course parts={course.parts} />
+    </>
+  )
+}
+
+export default App;
